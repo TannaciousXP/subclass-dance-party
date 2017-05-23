82ebdf38d03181427dc0a3f6cfa8640812323bae
Hack Reactor Student <student@hackreactor.com>
2017-05-22 17:21:53 -0700

    Working Dancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,49 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
+//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+//   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
+//   var oldStep = blinkyDancer.step;
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+//   blinkyDancer.step = function() {
+//     // call the old version of step at the beginning of any call to this new version of step
+//     oldStep();
+//     // toggle() is a jQuery method to show/hide the <span> tag.
+//     // See http://api.jquery.com/category/effects/ for this and
+//     // other effects you can use on a jQuery-wrapped html tag.
+//     blinkyDancer.$node.toggle();
+//   };
 
-  return blinkyDancer;
+//   return blinkyDancer;
+// };
+
+
+/* pseudoclassical instantiation */
+
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  //passing in all paramenters from makeDancer and save into var args
+  var args = [].slice.call(arguments);
+
+  //passing in timeBtwnSteps from makeDancer save into var timeWait
+  // var timeWait = [].slice.call(arguments[2])[0];
+
+  //applying args from makeDancer to makeBlinkyDancer's prototype
+  Dancer.apply(this, args);
+  this.oldStep = this.step();
+  
+
+  //'this' === makeBlinkyDancer; invoke timeBtwnSteps function 
+  
+  // this.dance = setTimeout(this.step(), this.timeBetweenSteps);
+  this.step();
 };
+
+
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+BlinkyDancer.prototype.step = function() {
+  this.oldStep;  
+  this.$node.toggle();
+};
+
--- src/dancer.js
+++ src/dancer.js
@@ -30,3 +30,65 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   return dancer;
 };
+
+// verti = top || bottom
+// hori = left || right
+
+var Dancer = function(top, left, timeBetweenSteps) {
+  // this = Object.create(makeDancer.prototype);
+  
+  // this.top = top;
+  // this.left = left;
+  // this.step();
+  // this.setPosition(top, left);
+  
+  // getting the HTML tag with pseudoclassical 
+  this.$node = $('<span class="dancer"></span>');
+  
+  // grab the first two arguments to invoke on newDancer obj later on
+  // var positions = [].slice.call(arguments[0], arguments[2]);
+  // grab the last element of arguments to call on the this.step;
+  // var timeWait = [].slice.call(arguments[2])[0];
+  
+
+  // Invoke this.step.call with timeWait
+  // Invoke this.setPosition.apply with positions
+  // this.step.call(this, timeWait);
+  // this.step.call(this, timeWait);
+  
+  
+  // this.setPosition.apply(this, positions);
+  this.timeBetweenSteps = timeBetweenSteps;   
+  this.step();
+  this.setPosition(top, left);
+  // return this;
+};
+
+// ------- break --------
+
+Dancer.prototype.step = function() {
+  setTimeout(this.step, this.timeBetweenSteps);
+};
+
+Dancer.prototype.setPosition = function(top, left) {
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
